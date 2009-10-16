65785419487fabf8bbb40a588841f6807aaa8107
nchaimov <nchaimov@fab751ff-edd5-44c7-826f-13ac260a3a68>
2009-10-16 20:31:22 +0000

    track points and display on screen
    git-svn-id: https://subversion.assembla.com/svn/wii_finger_tracking@5 fab751ff-edd5-44c7-826f-13ac260a3a68

--- src/prototype/Hellthissucks.java
+++ src/prototype/Hellthissucks.java
@@ -1,6 +1,10 @@
 package prototype;
 
+import java.awt.Dimension;
 import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
 import motej.*;
 import motej.event.*;
 import motej.request.ReportModeRequest;
@@ -9,6 +13,19 @@ public class Hellthissucks {
 	static ArrayList<Mote> motes = new ArrayList<Mote>();
 	public static void main(String[] arrrgs) throws InterruptedException
 	{
+		
+		/*
+		JFrame frame = new JFrame("WiiMote Point Tracker");
+		SwingPointTracker tracker = new SwingPointTracker();
+		frame.add(tracker);
+		Dimension size = new Dimension(1024,768);
+		frame.setSize(size);
+		frame.setPreferredSize(size);
+		frame.setMinimumSize(size);
+		frame.pack();
+		frame.setVisible(true);
+		*/
+		
 		System.setProperty("bluecove.jsr82.psm_minimum_off", "true");
 		Mote mote = new Mote("00191da7389d");
 /*		MoteFinderListener listener = new MoteFinderListener() {
@@ -32,18 +49,22 @@ public class Hellthissucks {
 		System.out.println(motes.size());
 */		IrCameraListener cameraListener = new IrCameraListener() {
 			public void irImageChanged(IrCameraEvent evt){
-				System.out.println("X = " + evt.getIrPoint(0).x + ", Y = " + evt.getIrPoint(0).y);
+				//System.out.println("X = " + evt.getIrPoint(0).x + ", Y = " + evt.getIrPoint(0).y);
+				
 			}
 		};
 
 		//Mote mote = motes.get(0);
-		mote.enableIrCamera();
-		mote.setReportMode(ReportModeRequest.DATA_REPORT_0x36);
-		mote.rumble(2);
-		Thread.sleep(20001);
-		mote.setReportMode(ReportModeRequest.DATA_REPORT_0x36);
+		mote.enableIrCamera(IrCameraMode.FULL, IrCameraSensitivity.WII_LEVEL_5);
+		mote.setReportMode(ReportModeRequest.DATA_REPORT_0x3e);
+		//mote.rumble(2);
+		//Thread.sleep(20001);
+		//mote.setReportMode(ReportModeRequest.DATA_REPORT_0x36);
 		
-		mote.addIrCameraListener(cameraListener);
+		
+		PointTrackerUI ui = new PointTrackerUI(mote);
+		//mote.addIrCameraListener(cameraListener);
+		
 		Thread.sleep(6000001);
 	}
 }
