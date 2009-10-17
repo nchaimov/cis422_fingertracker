49f947ac4d53be83d8bedeb7104f9c606a8e085a
nchaimov <nchaimov@fab751ff-edd5-44c7-826f-13ac260a3a68>
2009-10-17 03:09:32 +0000

    Fix class name
    git-svn-id: https://subversion.assembla.com/svn/wii_finger_tracking@8 fab751ff-edd5-44c7-826f-13ac260a3a68

--- src/prototype/Driver.java
+++ src/prototype/Driver.java
@@ -1,70 +1,61 @@
 package prototype;
 
-import java.awt.Dimension;
 import java.util.ArrayList;
 
-import javax.swing.JFrame;
-
-import motej.*;
-import motej.event.*;
+import motej.IrCameraMode;
+import motej.IrCameraSensitivity;
+import motej.Mote;
+import motej.event.IrCameraEvent;
+import motej.event.IrCameraListener;
 import motej.request.ReportModeRequest;
 
-public class Hellthissucks {
+public class Driver {
 	static ArrayList<Mote> motes = new ArrayList<Mote>();
-	public static void main(String[] arrrgs) throws InterruptedException
-	{
-		
+
+	public static void main(String[] arrrgs) throws InterruptedException {
+
 		/*
-		JFrame frame = new JFrame("WiiMote Point Tracker");
-		SwingPointTracker tracker = new SwingPointTracker();
-		frame.add(tracker);
-		Dimension size = new Dimension(1024,768);
-		frame.setSize(size);
-		frame.setPreferredSize(size);
-		frame.setMinimumSize(size);
-		frame.pack();
-		frame.setVisible(true);
-		*/
-		
+		 * JFrame frame = new JFrame("WiiMote Point Tracker"); SwingPointTracker
+		 * tracker = new SwingPointTracker(); frame.add(tracker); Dimension size
+		 * = new Dimension(1024,768); frame.setSize(size);
+		 * frame.setPreferredSize(size); frame.setMinimumSize(size);
+		 * frame.pack(); frame.setVisible(true);
+		 */
+
 		System.setProperty("bluecove.jsr82.psm_minimum_off", "true");
 		Mote mote = new Mote("00191da7389d");
-/*		MoteFinderListener listener = new MoteFinderListener() {
-			
-			public void moteFound(Mote mote) {
-				System.out.println("Found mote: " + mote.getBluetoothAddress());
-				mote.rumble(2000l);
-				motes.add(mote);
-			}
-		};
-*/		
+		/*
+		 * MoteFinderListener listener = new MoteFinderListener() {
+		 * 
+		 * public void moteFound(Mote mote) { System.out.println("Found mote: "
+		 * + mote.getBluetoothAddress()); mote.rumble(2000l); motes.add(mote); }
+		 * };
+		 */
 
-		
-		
-/*		MoteFinder finder = MoteFinder.getMoteFinder();
-		finder.addMoteFinderListener(listener);
-		
-		finder.startDiscovery();
-		Thread.sleep(300000l);
-		finder.stopDiscovery();
-		System.out.println(motes.size());
-*/		IrCameraListener cameraListener = new IrCameraListener() {
-			public void irImageChanged(IrCameraEvent evt){
-				//System.out.println("X = " + evt.getIrPoint(0).x + ", Y = " + evt.getIrPoint(0).y);
-				
+		/*
+		 * MoteFinder finder = MoteFinder.getMoteFinder();
+		 * finder.addMoteFinderListener(listener);
+		 * 
+		 * finder.startDiscovery(); Thread.sleep(300000l);
+		 * finder.stopDiscovery(); System.out.println(motes.size());
+		 */IrCameraListener cameraListener = new IrCameraListener() {
+			public void irImageChanged(IrCameraEvent evt) {
+				// System.out.println("X = " + evt.getIrPoint(0).x + ", Y = " +
+				// evt.getIrPoint(0).y);
+
 			}
 		};
 
-		//Mote mote = motes.get(0);
+		// Mote mote = motes.get(0);
 		mote.enableIrCamera(IrCameraMode.FULL, IrCameraSensitivity.WII_LEVEL_5);
 		mote.setReportMode(ReportModeRequest.DATA_REPORT_0x3e);
-		//mote.rumble(2);
-		//Thread.sleep(20001);
-		//mote.setReportMode(ReportModeRequest.DATA_REPORT_0x36);
-		
-		
+		// mote.rumble(2);
+		// Thread.sleep(20001);
+		// mote.setReportMode(ReportModeRequest.DATA_REPORT_0x36);
+
 		PointTrackerUI ui = new PointTrackerUI(mote);
-		//mote.addIrCameraListener(cameraListener);
-		
+		// mote.addIrCameraListener(cameraListener);
+
 		Thread.sleep(6000001);
 	}
 }
