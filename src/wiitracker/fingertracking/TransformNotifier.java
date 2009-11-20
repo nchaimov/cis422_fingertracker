1f62f8a29453ec719d7e489c5ea00b914562e21a
areinder <areinder@fab751ff-edd5-44c7-826f-13ac260a3a68>
2009-11-20 01:53:21 +0000

    Added comments and methods.
    git-svn-id: https://subversion.assembla.com/svn/wii_finger_tracking@42 fab751ff-edd5-44c7-826f-13ac260a3a68

--- src/wiitracker/fingertracking/TransformNotifier.java
+++ src/wiitracker/fingertracking/TransformNotifier.java
@@ -9,7 +9,15 @@ import javax.swing.event.EventListenerList;
 import motej.IrPoint;
 import motej.event.IrCameraEvent;
 import motej.event.IrCameraListener;
-
+/**
+ * 
+ * This class wraps the PerspectiveTransform class for detail hiding and to make it a member of the pipeline.
+ * PerspectiveTransform creates a projective transform from one plane in space to another. Specifically, since the map is identical to the image, it takes any point from the camera, and returns its location relative to the map.
+ * 
+ * 
+ * @author areinder
+ *
+ */
 public class TransformNotifier implements IrCameraListener, IrCameraNotifier {
 	
 	private static final TransformNotifier INSTANCE = new TransformNotifier(); 
@@ -27,11 +35,41 @@ public class TransformNotifier implements IrCameraListener, IrCameraNotifier {
 
 	}
 
-	public void setTransform(PerspectiveTransform p) {
+	private void setTransform(PerspectiveTransform p) {
 		transform.setTransform(p);
 	}
+	
+	
+	/**
+	 * Sets the transform to transform from four selected points on the physical map to the same four points on the map image.
+	 * Points should be given either clockwise or counter-clockwise around the map, starting from any corner. 
+	 * @param fromMote 	Point2D data from the camera for the four corners.
+	 * @param fromMap	Pixel locations for the four corners.
+	 */
+	public void setTransform(Point2D[] fromMote, Point2D[] fromMap) {
+		this.setTransform(PerspectiveTransform.getQuadToQuad(
+				fromMote[0].getX(), fromMote[0].getY(),
+				fromMote[1].getX(), fromMote[1].getY(),
+				fromMote[2].getX(), fromMote[2].getY(),
+				fromMote[3].getX(), fromMote[3].getY(), 
+				
+				fromMap[0].getX(), fromMap[0].getY(),
+				fromMap[1].getX(), fromMap[1].getY(),
+				fromMap[2].getX(), fromMap[2].getY(),
+				fromMap[3].getX(), fromMap[3].getY()));
+	}
 
+	/**
+	 * Transforms the points in array points using the current transform.
+	 * Transforms them in-place, and returns the array that was passed in.
+	 * @param points Points to be transformed.
+	 * @return points - the transformed points, in the same array.
+	 */
 	public Point2D[] transform(Point2D[] points) {
+		Point2D[] newPoints = points.clone();
+		for (int i = 0; i < newPoints.length; i++) {
+			newPoints[i] = (Point2D) points[i].clone();
+		}
 		transform.transform(points, 0, points, 0, points.length);
 		return points;
 	}
